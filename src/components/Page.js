d74979f41acb26c5dd73ce41194053c99e13aef6
lyndonrey <lyndonreybualat@gmail.com>
2022-12-05 21:23:55 -0700

    Update from laptop

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Page() {
   return (
-    <div className='text-teal-700'>
+    <div className='text-teal-800'>
         <div className='max-w-[800px] mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
             <p>Hello</p>
-            <h1>There</h1>
+            <h1>My Name is Lyndon</h1>
         </div>
     </div>
   )
