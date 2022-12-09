d854309d20b424cc76d5177cc64e1be2795e1511
Lyndon Rey Bualat <lyndonreybualat@gmail.com>
2022-12-09 00:27:34 -0700

    Added more styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,43 @@
 import React, { useState } from 'react'
 import {RxCross2, RxTokens} from 'react-icons/rx'
 
+<RxTokens.Provider value={{ color: "blue", className: "global-class-name" }}>
+
+</RxTokens.Provider>;
+
 function Navbar() {
     const [nav,setNav] = useState(false)
     const handleNav = () => {
         setNav(!nav)
     }
   return (
-    <div className='flex justify-between items-center mx-auto px-4 pt-2 max-w-[1240px] text-brown'> {/*Desktop Menu*/}
+    <div className='flex justify-between items-center mx-auto px-4 pt-2 max-w-[1240px] text-brown'>  {/*Desktop Menu*/}
       <img src='../logo.png' alt='logo' className='max-w-[90px]'></img>
-      <ul className='hidden md:flex'>
+      <ul className='hidden md:flex text-white font-medium'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Projects</li>
         <li className='p-4'>Contact</li>
         <li className='p-4'>About</li>
       </ul>
 
-      <div onClick={handleNav} className="block md:hidden">   {/*Mobile Menu*/}
-        {!nav ? <RxCross2 size={35}/> : < RxTokens size={35}/>}
+      <div onClick={handleNav} className="block md:hidden">    {/*Mobile Menu*/}
+        {!nav ? <RxCross2 size={35} color="#FCD34D"/> : < RxTokens size={35} color="#FCD34D"/>}
       </div>
-      <div className= {!nav? 'w-[50%] h-full fixed right-0 top-0 border-r border-teal-700 bg-[#bbd5db] ease-in-out duration-500 ' : 'fixed right-[-100%] '}>
+      <div className= {!nav? 'w-[100%] h-[38%] fixed right-0 top-0 border- bg-[#415a77] ease-in-out duration-500 ' : 'fixed right-[-100%] '}>
         <div onClick={handleNav} className="block md:hidden p-6">   {/*Mobile Menu*/}
-        {!nav ? <RxCross2 size={35}/> : < RxTokens size={35}/>}
+        {!nav ? <RxCross2 size={35} color="#FCD34D"/> : < RxTokens size={35} color="#FCD34D"/>}
       </div>
-        <ul className='p-4 uppercase '>
-            <li className='p-4 border-b border-teal-800'>Home</li>
-              <li className='p-4 border-b border-teal-800'>Projects</li>
-            <li className='p-4 border-b border-teal-800'>Contact</li>
+        <ul className='p-4 uppercase font-medium text-white'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Projects</li>
+            <li className='p-4'>Contact</li>
             <li className='p-4'>About</li> 
         </ul>
       </div>
     </div>
   );
 };
+
+
 
 export default Navbar
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import Typed from 'react-typed';
 
 function Page() {
   return (
-    <div className='text-teal-800'>
-        <div className='max-w-[800px] mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p>Hello</p>
-            <h1>My Name is Lyndon</h1>
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className="md:text-8xl sm:text-7xl text-5xl font-bold text-amber-300 pb-5">
+        <Typed
+        strings={['Hello']}
+        typeSpeed={140} 
+        backSpeed={140}
+        loop
+        />
         </div>
+          <h1 className="font-bold text-xl md:text-4xl">I'm Lyndon a Web Developer based in Canada</h1>
+          <p className="text-gray-400 text-xl font-bold md:text-2xl">I design and build websites</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
